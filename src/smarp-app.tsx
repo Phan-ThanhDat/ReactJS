93eee2f57efa84cc8aa196e84fd37b41d5eb726f
hongduc-phan <phduc.phanhongduc@gmail.com>
2020-10-08 16:18:01 +0300

    change Smarp component to function component

--- src/smarp-app.tsx
+++ src/smarp-app.tsx
@@ -13,7 +13,7 @@ export interface ISmarp {
   posts: IPost['post'][];
   isOpen: boolean;
 }
-export class SmarpApp extends React.Component<{}, ISmarp> {
+export default class SmarpApp extends React.Component<{}, ISmarp> {
   constructor(props: any) {
     super(props);
 
@@ -78,10 +78,11 @@ export class SmarpApp extends React.Component<{}, ISmarp> {
       likesCount: 0,
     });
 
+    // should not set-state 2 times, because the component will re-render 2 times
     this.setState({
       posts: updatedPosts.reverse(),
     });
-
+    console.log('13111.');
     this.setState({
       isOpen: false,
     });
@@ -137,3 +138,101 @@ export class SmarpApp extends React.Component<{}, ISmarp> {
     );
   }
 }
+
+// export default function SmarpApp() {
+//   const [posts, setPosts] = React.useState<IPost['post'][]>([]);
+//   const [isOpen, setIsOpen] = React.useState<boolean>(false);
+
+//   React.useEffect(() => {
+//     fetch('https://jsonplaceholder.typicode.com/posts').then((response) => {
+//       const jsonResponse = response.json();
+
+//       jsonResponse.then((rawData) => {
+//         const data = [];
+
+//         for (let i = 0; i < rawData.length; i++) {
+//           const updatedPost = rawData[i];
+//           updatedPost.likesCount = 0;
+
+//           data.push(updatedPost);
+//         }
+
+//         setPosts(data);
+//       });
+//     });
+//   }, []);
+
+//   function onLike(index: number) {
+//     // Should clone to new array, because when set-state, the posts array has the same
+//     // address with this.state.posts, so the component will not re-render
+//     // even we did modify something.
+//     const postsClone = posts.slice();
+//     const post = postsClone[index];
+
+//     if (post.isLiked) {
+//       post.likesCount = post.likesCount - 1;
+//       post.isLiked = false;
+//     } else {
+//       post.likesCount = post.likesCount + 1;
+//       post.isLiked = true;
+//     }
+//     setPosts(postsClone);
+//   }
+
+//   function onSubmit(payload: { title: string; body: string }) {
+//     const updatedPosts = posts.slice();
+
+//     // Missing type of IPost["post"]
+//     updatedPosts.push({
+//       title: payload.title,
+//       body: payload.body,
+//       isLiked: false,
+//       likesCount: 0,
+//     });
+//     console.log('111..');
+//     setPosts(updatedPosts.reverse());
+//     setIsOpen(false);
+//   }
+
+//   // Should define function here to maintain/refactor easily
+//   function onCreatingPost() {
+//     setIsOpen(true);
+//   }
+
+//   // Should define function here to maintain/refactor easily
+//   function onModal() {
+//     setIsOpen(false);
+//   }
+
+//   return (
+//     <div>
+//       <header className={styles.header}>
+//         <div className={`container ${styles.headerImageWrapper}`}>
+//           <img src={logo} className={styles.headerImage} alt='logo' />
+//         </div>
+//       </header>
+
+//       <div className={`container ${styles.main}`}>
+//         <div className={styles.buttonWrapper}>
+//           <Button onClick={onCreatingPost}>Create a post</Button>
+//         </div>
+
+//         <PostList posts={posts} onLike={onLike} />
+//       </div>
+
+//       <Modal
+//         isOpen={isOpen}
+//         className={styles.reactModalContent}
+//         overlayClassName={styles.reactModalOverlay}
+//       >
+//         <div className={styles.modalContentHelper}>
+//           <div className={styles.modalClose} onClick={onModal}>
+//             <FaTimes />
+//           </div>
+
+//           <Form on-submit={onSubmit} />
+//         </div>
+//       </Modal>
+//     </div>
+//   );
+// }
